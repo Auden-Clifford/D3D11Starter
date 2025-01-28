be1aa56db00d9ab30bceb556665b73e06369d507
Auden-Clifford <awpclifford@gmail.com>
2025-01-27 20:06:02 -0500

    Created Mesh.h

---
+++ Mesh.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <d3d11.h>
+#include <wrl/client.h>
+#include "Vertex.h"
+#include "Graphics.h"
+
+class Mesh
+{
+public:
+	// OOP stuff
+	Mesh(Vertex* a_pVerticies, unsigned int a_uVerticiesLength, unsigned int* a_pIndicies, unsigned int a_uIndiciesLength);
+	~Mesh();
+
+	// primary functions
+	Microsoft::WRL::ComPtr<ID3D11Buffer> GetVertexBuffer();
+	Microsoft::WRL::ComPtr<ID3D11Buffer> GetIndexBuffer();
+
+	unsigned int GetIndexCount();
+	unsigned int GetVertexCount();
+	void Draw();
+
+private:
+	// geometry data buffers
+	Microsoft::WRL::ComPtr<ID3D11Buffer> m_cpVertexBuffer;
+	Microsoft::WRL::ComPtr<ID3D11Buffer> m_cpIndexBuffer;
+
+	unsigned int m_nIndicies; //number of indices in index buffer
+	unsigned int m_nVertices; //number of vertices in vertex buffer
+	unsigned int m_nFaces;
+};
